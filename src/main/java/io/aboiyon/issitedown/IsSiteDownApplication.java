e935884afe5b6521c11b29de03890e2c9ee07f34
Kiprotich Michael <kiprotichmicheal570@gmail.com>
2022-10-06 17:55:26 +0300

    initial commit

---
+++ src/main/java/io/aboiyon/issitedown/IsSiteDownApplication.java
@@ -0,0 +1,13 @@
+package io.aboiyon.issitedown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsSiteDownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsSiteDownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/aboiyon/issitedown/IsSiteDownApplicationTests.java
@@ -0,0 +1,13 @@
+package io.aboiyon.issitedown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsSiteDownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
